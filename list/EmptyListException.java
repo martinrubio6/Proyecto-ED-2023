eeb95258a665d54cbd8ffd915182f6922baf2d0f
Martin Rubio <71666017+martinrubio6@users.noreply.github.com>
2023-11-16 18:58:01 -0300

    Add files via upload

---
+++ list/EmptyListException.java
@@ -0,0 +1,16 @@
+package list;
+
+public class EmptyListException extends Exception {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyListException(String msg) {
+		super(msg);
+	}
+
+
+}
